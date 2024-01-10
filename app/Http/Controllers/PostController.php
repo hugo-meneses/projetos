0261bfd726dbe508102bc7c919a32104b36f5d6b
hugo.meneses <hugo.menes@gmail.com>
2024-01-09 21:26:20 -0300

    Novo blog em construcao 2024

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+
+class PostController extends Controller
+{
+    public function index() {
+        $posts = Post::orderBy('created_at', 'desc')->get();
+        return view('posts.index', ['posts' => $posts]);
+    }
+
+    public function create() {
+        return view('posts.create');
+    }
+    public function store(Request $request) {
+        // validations
+        $request->validate([
+          'title' => 'required',
+          'description' => 'required',
+          'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        ]);
+      
+        $post = new Post;
+      
+        $file_name = time() . '.' . request()->image->getClientOriginalExtension();
+        request()->image->move(public_path('images'), $file_name);
+      
+        $post->title = $request->title;
+        $post->description = $request->description;
+        $post->image = $file_name;
+      
+        $post->save();
+        return redirect()->route('posts.index')->with('success', 'Post criado com sucesso.');
+    }
+}
